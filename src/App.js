685df4b9db8362c3129753ff2adf092fcdb7d7c3
ihor-vip <vip20140902@gmail.com>
2021-08-18 15:39:46 +0300

    lesson5-homework

--- src/App.js
+++ src/App.js
@@ -1,12 +1,61 @@
-import {Form} from "./components/form/Form";
-
-function App() {
-
-  return (
-      <div>
-         <Form/>
-      </div>
-  );
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
 }
 
-export default App;
+from "react-router-dom";
+
+import Users from "./components/Users";
+import Posts from "./components/posts/Posts";
+import Comments from "./components/comments/Comments";
+import './App.css'
+
+
+
+export default function App() {
+    return (
+        <Router>
+            <div className={'wrap'}>
+                <div className={'box1'}><Link to={'/'}>RELOAD</Link>
+                    <br/></div>
+
+                <div className={'box1'}><Link to={'/users-page'} target={'_blank'}>users</Link>
+                    <br/></div>
+
+               <div className={'box1'}> <Link to={'/posts-page'} target={'_blank'}>posts</Link>
+                   <br/></div>
+
+              <div className={'box1'}>  <Link to={'/comments-page'} target={'_blank'}>comments</Link>
+                  <br/></div>
+
+                <h2><b><i>Choose the info page</i></b></h2>
+            </div>
+
+
+            <div>
+
+                <Switch>
+
+                    <Route path={'/users-page'} component={Users}/>
+
+                    <Route path={'/posts-page'} component={Posts}/>
+
+                    <Route path={'/comments-page'} component={Comments}/>
+
+                    {/*<Route path={'/posts-page'} render={(props) => {*/}
+
+                    {/*    return <Posts/>*/}
+                    }}/>
+
+
+                </Switch>
+
+
+            </div>
+
+
+        </Router>
+    );
+}
