01ef119a5a2c7f1ed04a4ae10971c98b582d6704
ihor-vip <vip20140902@gmail.com>
2021-08-29 10:47:59 +0300

    lesson7- classwork

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
-import {useReducer} from "react";
-import reducer from "../src/components/reducers/reducers";
-import './App.css'
+import {Provider} from "react-redux";
+import UserContainer from "./components/UserContainer";
+import store from './redux/store'
 
 export default function App() {
 
-    let [{a, b, c}, dispatch] = useReducer(reducer, {a: 0, b: 0, c: 0});
-
-    return (
-        <div className={'main-wrap'}>
-            <h2>NUMBER a-{a} b-{b} c-{c}</h2>
-                <div>
-                    <button onClick={() => dispatch({obj: '1', act: '+'})}>increase <b>a</b> by 1</button>
-                    <button onClick={() => dispatch({obj: '1', act: '-'})}>decrease <b>a</b> by 1</button>
-                </div>
-
-                <div>
-                    <button onClick={() => dispatch({obj: '2', act: '+'})}>increase <b>b</b> by 1</button>
-                    <button onClick={() => dispatch({obj: '2', act: '-'})}>decrease <b>b</b> by 1</button>
-                </div>
-
-                <div>
-                    <button onClick={() => dispatch({obj: '3', act: '+'})}>increase <b>c</b> by 1</button>
-                    <button onClick={() => dispatch({obj: '3', act: '-'})}>decrease <b>c</b> by 1</button>
-                </div>
-        </div>
-    );
+     return (
+         <Provider store={store}>
+                 <div>
+                     <UserContainer/>
+                 </div>
+         </Provider>
+      )
 }
